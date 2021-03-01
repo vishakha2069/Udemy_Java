44d170f4ea21bc5bc71c5a1063b006cf3f725bbd
Vishakha <74141956+vishakha2069@users.noreply.github.com>
2021-03-01 22:01:03 +0530

    Create ReadKeyboard.java

---
+++ ReadKeyboard.java
@@ -0,0 +1,18 @@
+# Udemy_Java
+import java.util.*;
+
+public class ReadKeyboard {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int dd, mm, yy;
+        System.out.println("Enter date, month, year: ");
+        dd=sc.nextInt();
+        mm=sc.nextInt();
+        yy=sc.nextInt();
+        System.out.println("Today's date");
+
+    }
+
+}
