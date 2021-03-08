e597eccaab095b05ad0ae7821ad5dc9f0ec94e5c
Vishakha <74141956+vishakha2069@users.noreply.github.com>
2021-03-08 19:40:47 +0530

    Create VariableNames.java

---
+++ VariableNames.java
@@ -0,0 +1,16 @@
+# Udemy_Java
+package variablenames;
+
+public class VariableNames {
+
+    public static void main(String[] args) {
+        //int x;
+        //int roll_number$student;
+        //int _x;
+        
+        //int rollNumberOfStudent;
+        //float averageMarksOfClass;
+        
+    }
+    
+}
